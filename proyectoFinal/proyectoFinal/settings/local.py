444513c989da5fef0011d0bb9ae877eabb422bd9
Joaquin Strusiat <joaquinpjs@gmail.com>
2023-07-18 15:05:36 -0300

    15.05

--- proyectoFinal/proyectoFinal/settings/local.py
+++ proyectoFinal/proyectoFinal/settings/local.py
@@ -7,21 +7,10 @@ ALLOWED_HOSTS = ['*']
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-''' 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'proyectoinfo2023',
-        'USER': 'root',
-        'PASSWORD': '44682545Mysql',
-        'HOST': 'localhost',
-    }
-}
-
