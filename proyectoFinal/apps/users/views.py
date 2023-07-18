4c4a748771cddb2bd6e0141190ecd8aee7c75fbe
Joaquin Strusiat <joaquinpjs@gmail.com>
2023-07-18 17:23:27 -0300

    17.23

--- proyectoFinal/apps/users/views.py
+++ proyectoFinal/apps/users/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def Users(request):
 
-	return render(request, 'app_users/users.html')
+	return render(request, 'apps_users/users.html')
 
