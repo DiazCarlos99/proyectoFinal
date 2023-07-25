733a952bb35cade380a1803f4d8b9b93ad1be930
diaz carlos <carlosrdiaz99@gmail.com>
2023-07-25 17:15:49 -0300

    17:15/25

--- proyectoFinal/apps/empr/urls.py
+++ proyectoFinal/apps/empr/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from . import views
 
+
 app_name='empr'
 
 urlpatterns = [
+    
     path('Crear', views.CrearEmpredimiento.as_view(), name="crear_empredimiento"),
     
     path('Listar', views.ListarEmprendimientos.as_view(), name="listar_emprendimientos"),
@@ -22,4 +24,5 @@ urlpatterns = [
     
     path('Filtrar/alfabetico/<str:orden>/', views.FiltrarPorOrdenAlfabetico.as_view(), name='emprendimiento_alfabetico'),
      
+    
 ] 
